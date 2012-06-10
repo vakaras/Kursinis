023a0da1780cfdecef04346370b0eacf021b4afc
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-06-10 11:38:54 +0300

    Pridėtas pavyzdys su kompiliatoriumi.

---
+++ content/code/Compiler1.scala
@@ -0,0 +1,6 @@
+scala> import scala.tools.nsc.{Global => ScalaCompiler, Settings}
+import scala.tools.nsc.{Global=>ScalaCompiler, Settings}
+
+scala> val compiler = new ScalaCompiler(new Settings())
+compiler: scala.tools.nsc.Global = scala.tools.nsc.Global@????????
+
